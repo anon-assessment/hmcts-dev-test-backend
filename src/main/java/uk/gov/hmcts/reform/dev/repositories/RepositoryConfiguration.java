b07c17e7e1c008d365239ae95ae1a91d69005895
anon-assessment <229321666+anon-assessment@users.noreply.github.com>
2025-09-30 18:21:06 +0100

    Added Swagger and prevented repositories being available directly

---
+++ src/main/java/uk/gov/hmcts/reform/dev/repositories/RepositoryConfiguration.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.reform.dev.repositories;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RepositoryConfiguration implements RepositoryRestConfigurer {
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.disableDefaultExposure();
+    }
+}
